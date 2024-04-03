b5d41594a84023ff1142d67bc3f856f04bbd560e
Ricky <88468957+ramirez0777@users.noreply.github.com>
2024-04-03 17:59:17 -0600

    Module 2

--- routes/products.js
+++ routes/products.js
@@ -2,5 +2,32 @@ var express = require("express");
 var router = express.Router();
 
 module.exports = function (db) {
+  router
+    .route('/products')
+    .get((req, res) => {
+      res.send(db.get('products').value());
+    })
+    .post((req, res) => {
+      const newProduct = req.body;
+      res.send(db.get('products').insert(newProduct).write());
+    });
+
+  router.patch("/products/:id", (req, res) => {
+    res.send(db.get('products').find({id: req.params.id}).assign(req.body).write()) ;
+  });
+
+  router.delete("/products/:id", (req, res) => {
+    res.send(
+      db.get('products').remove({id : req.params.id}).write()
+    );
+  });
+
+  router.get('/products/:id', (req, res) => {
+    res.send(
+      db.get('products').find({id : req.params.id}).value()
+    );
+  });
+  
+
   return router;
 };
